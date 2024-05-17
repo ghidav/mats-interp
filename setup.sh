70cef1bc94a3c94f05bab1361f653c2ee658fca1
ghidav <davide.ghilardi0@gmail.com>
2024-05-17 07:47:05 +0000

    Setup

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Set up a Python virtual environment
+echo "Creating a virtual environment..."
+python3 -m venv myenv
+
+# Activate the virtual environment
+source myenv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install necessary packages
+echo "Installing necessary packages..."
+pip install -r requirements.txt
+pip install -U "huggingface_hub[cli]"
+
+git clone git@github.com:ghidav/feature-circuits.git
+
+# Prompt the user for Hugging Face token
+read -p "Please enter your Hugging Face token: " HF_TOKEN
+
+# Set the Hugging Face token as an environment variable
+export HF_TOKEN=$HF_TOKEN
+echo "Hugging Face token set."
+
+# Downloading models
+huggingface-cli download gpt2
+huggingface-cli download google/gemma-2b
+huggingface-cli download jbloom/Gemma-2b-Residual-Stream-SAEs
+huggingface-cli download jbloom/GPT2-Small-SAEs-Reformatted
+
+# Prompt the user for Weights & Biases token
+read -p "Please enter your Weights & Biases token: " WANDB_TOKEN
+
+# Initialize Weights & Biases with the provided token
+wandb login $WANDB_TOKEN
+echo "Weights & Biases initialized."
+
+# Deactivate the virtual environment
+deactivate
+
+echo "Setup completed successfully."
