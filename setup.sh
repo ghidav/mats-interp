005de3c065ec0c2961da9f9aa976a3242a3a7a64
ghidav <davide.ghilardi0@gmailcom>
2024-05-18 11:16:31 +0000

    modified:   experiments/002-task-vectors/002-tv.ipynb 	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,51 +1,39 @@
 #!/bin/bash
 
-# Set up a Python virtual environment
-echo "Creating a virtual environment..."
-python3 -m venv ../myenv
-
-# Activate the virtual environment
-source ../myenv/bin/activate
-
 # Upgrade pip
 pip install --upgrade pip
 
 # Install necessary packages
 echo "Installing necessary packages..."
-#pip install -r requirements.txt
+pip install -r requirements.txt
 pip install -U "huggingface_hub[cli]"
 pip install -U kaleido
 python -m ipykernel install --user --name=myenv --display-name="mats-interp"
 
 git clone git@github.com:ghidav/feature-circuits.git
 
-# Prompt the user for Hugging Face token
-read -p "Please enter your Hugging Face token: " HF_TOKEN
+cd PySvelte
+pip install -e .
+cd ..
 
 # Set the Hugging Face token as an environment variable
-export HF_TOKEN=$HF_TOKEN
+CONFIG_FILE="mats-interp/keys.json"
+HF_TOKEN=$(jq -r '.HF_TOKEN' "$CONFIG_FILE")
+export HF_TOKEN
+
 mkdir /workspace/huggingface
 export HF_HOME=/workspace/huggingface
 echo "Hugging Face token set."
 
 # Downloading models
+huggingface-cli login --token HF_TOKEN
 huggingface-cli download gpt2
 huggingface-cli download google/gemma-2b
 huggingface-cli download jbloom/Gemma-2b-Residual-Stream-SAEs
 huggingface-cli download jbloom/GPT2-Small-SAEs-Reformatted
 
 # Prompt the user for Weights & Biases token
-read -p "Please enter your Weights & Biases token: " WANDB_TOKEN
-
+WANDB_TOKEN=$(jq -r '.WANDB_TOKEN' "$CONFIG_FILE")
 # Initialize Weights & Biases with the provided token
 wandb login $WANDB_TOKEN
 echo "Weights & Biases initialized."
-
-apt update
-apt-get install sudo -y
-sudo apt install vim
-
-configure_git
-export EDITOR="vim"
-
-echo "Setup completed successfully."
